e87a75715caa0b1c378c6869c4d0eb00ea627519
DanyaPe <denzed5221@gmail.com>
2025-12-15 21:47:44 +0700

    feat: Добавление элемента ожидания загрузки данных (Preloader)
    В состоянии основного компонента приложения (App) установлен признак загрузки (loading), которые передается в главный компонент (Main) и отображает компонент загрузки (Preloader) в зависимости от значения признака

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,23 @@ import fetchMovieList from "./utils/fetchMovieList";
 export default class App extends React.Component {
     state = {
         movieList: [],
+        loading: true,
     };
 
     async componentDidMount() {
         const result = await fetchMovieList("Star Wars");
         if (result && result.Response === "True")
-            this.setState({ movieList: result.Search });
+            this.setState({ movieList: result.Search, loading: false });
     }
 
     render() {
         return (
             <div className="min-w-full min-h-screen flex flex-col">
                 <Header />
-
-                <Main movieList={this.state.movieList} />
-
+                <Main
+                    movieList={this.state.movieList}
+                    loading={this.state.loading}
+                />
                 <Footer />
             </div>
         );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,15 @@
 import "tailwindcss";
 import SearchInput from "./SearchInput";
 import Movies from "./Movies";
+import Preloader from "./Preloader";
 
 export default function Main(props) {
-    const { movieList } = props;
+    const { movieList, loading } = props;
 
     return (
         <main className="bg-gray-800 flex flex-col grow items-center py-10">
             <SearchInput />
-            <Movies movieList={movieList} />
+            {loading ? <Preloader /> : <Movies movieList={movieList} />}
         </main>
     );
 }
