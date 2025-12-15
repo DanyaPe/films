d266f27a86fc7d0dc55ecc3a8c57ffe34a3d37cf
DanyaPe <denzed5221@gmail.com>
2025-12-15 19:45:06 +0700

    feat: Добавление проверки на доступность постера в компоненте карточки фильма (Movie)

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -5,7 +5,15 @@ export default function Movie(props) {
 
     return (
         <div className="w-80 h-130 m-10">
-            <img src={Poster} alt={Title} className="w-80 h-120 object-fill" />
+            <img
+                src={
+                    Poster == "N/A"
+                        ? "https://placehold.jp/535365/ffffff/320x480.png?text=Movie%20don't%20have%20a%20poster"
+                        : Poster
+                }
+                alt={Title}
+                className="w-80 h-120 object-fill"
+            />
             <p className="text-amber-50 line-clamp-2 h-12 font-spotify">
                 {Title}
             </p>
