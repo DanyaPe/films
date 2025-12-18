683176e1769c336ebcb3d8e348fcfca602bee660
DanyaPe <denzed5221@gmail.com>
2025-12-18 13:55:21 +0700

    fix: Исправление алгоритма запроса, отсутствовал оборот await для обработки Promise, вынос публичного ключа в локальный файл .env

--- src/utils/fetchMovieList.js
+++ src/utils/fetchMovieList.js
@@ -1,31 +1,31 @@
 const fetchMovieList = async (title, type, page) => {
-    const result = [];
-
     if (
         typeof title !== "string" ||
         typeof type !== "string" ||
         typeof page !== "string"
-    ) {
+    )
         console.error(
             `В качестве параметра передан не соответствующий тип данных`
         );
-    }
 
+    const API_KEY = import.meta.env.VITE_API_KEY;
+    const endPoint = `http://www.omdbapi.com/?apikey=${API_KEY}&s=${title}&type=${type}&page=${page}&r=json`;
+    const result = [];
     title = title.split(" ");
 
     try {
-        const response = await fetch(
-            `http://www.omdbapi.com/?apikey=d45a732e&s=${title}&type=${type}&page=${page}&r=json`
-        );
+        const response = await fetch(endPoint);
 
-        if (response.ok) {
-            console.error(`Ошибка HTTP запроса:\n${response.status}`);
+        if (!response.ok) {
+            console.error(
+                `Ошибка HTTP запроса:\n${endPoint} - ${response.status}`
+            );
         } else {
-            const json = response.json();
+            const json = await response.json();
             json.Response === "True"
                 ? result.push(...json.Search)
                 : console.warn(
-                      `Не удалось обработать ответ от сервиса OMDb:\n${json.Error}`
+                      `Не удалось обработать ответ от сервиса OMDb:\n${endPoint} - ${json.Error}`
                   );
         }
     } catch (error) {
