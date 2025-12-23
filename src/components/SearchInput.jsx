bba878a290f4213c393b83a7891542b50c056947
DanyaPe <denzed5221@gmail.com>
2025-12-24 00:03:13 +0700

    style: Изменение размерности строки поиска (SearchInput) для экранов до 768px, корректировка цвета подсказки

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -13,7 +13,7 @@ export default class SearchInput extends React.Component {
         return (
             <form
                 onSubmit={(event) => event.preventDefault()}
-                className="relative p-2 min-w-[20vw] max-w-[80vw] flex items-center gap-x-2 md:w-[50vw] mt-2 md:mt-5"
+                className="relative p-2 flex items-center gap-x-2 w-[90vw] md:w-[50vw] mt-2 md:mt-5"
             >
                 <img
                     src="./loupe.svg"
@@ -37,7 +37,7 @@ export default class SearchInput extends React.Component {
                             handleSearch(this.state.text);
                     }}
                 />
-                <span class="pointer-events-none absolute top-1/2 -translate-y-1/2 text-cyan-900 transition-all duration-300 peer-focus:top-0 peer-not-placeholder-shown:top-0 text-[8px] left-9 md:text-xs md:left-10 lg:text-sm xl:left-12 xl:text-base">
+                <span class="pointer-events-none absolute top-1/2 -translate-y-1/2 dark:text-gray-400 text-cyan-900 transition-all duration-300 peer-focus:top-0 peer-not-placeholder-shown:top-0 text-[8px] left-9 md:text-xs md:left-10 lg:text-sm xl:left-12 xl:text-base">
                     {text(language, "tip_for_search")}
                 </span>
             </form>
