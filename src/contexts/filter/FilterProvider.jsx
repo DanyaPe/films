7afa3768759e3fab2ff56c791862c47bd0f0ab72
DanyaPe <denzed5221@gmail.com>
2026-01-03 15:40:05 +0700

    refactor: Вынесение списка фильтров(FILTERS) в отдельную константу

--- src/contexts/filter/FilterProvider.jsx
+++ src/contexts/filter/FilterProvider.jsx
@@ -2,11 +2,10 @@ import { useState } from "react";
 import { FilterContext } from "./FilterContext.js";
 
 export function FilterProvider({ children }) {
-    const FILTERS = ["all", "movie", "series"];
     const [filter, setFilter] = useState("all");
 
     return (
-        <FilterContext.Provider value={{ filter, setFilter, FILTERS }}>
+        <FilterContext.Provider value={{ filter, setFilter }}>
             {children}
         </FilterContext.Provider>
     );
