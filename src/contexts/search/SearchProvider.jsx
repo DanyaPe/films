72c8dc7cb07618928c6299b9f0cb893d90118e88
DanyaPe <denzed5221@gmail.com>
2026-01-10 19:40:14 +0700

    refactor: Переработка обработки состояния провайдера поиска(SearchProvider)
    В провайдере поиска(SearchProvider) изменена логика обработки состояния и управления поиска с помощью хука useReducer, логика обработки списка фильмов и поиска фильмов разделены друг от друга

--- src/contexts/search/SearchProvider.jsx
+++ src/contexts/search/SearchProvider.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import { SearchContext } from "./SearchContext.js";
 import { useFilter } from "../filter/FilterContext.js";
 import { useMovieList } from "../movieList/MovieListContext.js";
@@ -7,30 +7,69 @@ import { fetchResultList } from "../../utils/fetchMovieList.js";
 import sleep from "../../utils/sleep.js";
 
 export function SearchProvider({ children }) {
-    const [title, setTitle] = useState(" ");
-    const [isLoading, setLoading] = useState(false);
-    const [page, setPage] = useState(0);
-    const [isEnd, setEnd] = useState(false);
     const { filter } = useFilter();
-    const { movieList, setMovieList } = useMovieList();
+    const { setMovieList } = useMovieList();
     const { setFirstRender } = useTheme();
+
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case "Search_start":
+                return {
+                    ...state,
+                    title: action.payload.title,
+                    page: 1,
+                    isLoading: true,
+                    isEnd: false,
+                };
+            case "Search_continue":
+                return {
+                    ...state,
+                    page: state.page + 1,
+                    isLoading: true,
+                };
+            case "Search_end":
+                return {
+                    ...state,
+                    isLoading: false,
+                    isEnd: action.payload.isEnd,
+                };
+            default:
+                return state;
+        }
+    };
+
+    const [state, dispatch] = useReducer(reducer, {
+        title: "",
+        isLoading: false,
+        page: 0,
+        isEnd: false,
+    });
 
     const handleSearch = async (currentText) => {
         setFirstRender(false);
         await sleep(1000);
-        setLoading(true);
 
-        const isAnotherTitle = title !== currentText;
-        if (isAnotherTitle) setTitle(currentText);
+        const isAnotherTitle = state.title !== currentText;
+        let currentPage = state.page;
 
-        const result = await fetchResultList(
-            currentText,
-            String(isAnotherTitle ? 1 : page + 1),
-            filter
-        );
+        if (isAnotherTitle) {
+            setMovieList([]);
+            dispatch({
+                type: "Search_start",
+                payload: { title: currentText },
+            });
+            currentPage = 0;
+        } else {
+            dispatch({
+                type: "Search_continue",
+            });
+            currentPage++;
+        }
 
-        setMovieList(
-            (isAnotherTitle ? [] : movieList).concat(
+        const result = await fetchResultList(currentText, currentPage, filter);
+
+        setMovieList((prev) =>
+            prev.concat(
                 Array.from(
                     new Map(
                         result.map((movie) => [movie.imdbID, movie])
@@ -38,17 +77,22 @@ export function SearchProvider({ children }) {
                 )
             )
         );
-        setPage(isAnotherTitle ? 1 : page + 1);
-        setEnd(result.length < (filter === "all" ? 20 : 10) ? true : false);
-        setLoading(false);
+        dispatch({
+            type: "Search_end",
+            payload: {
+                newMovie: result,
+                isEnd:
+                    result.length < (filter === "all" ? 20 : 10) ? true : false,
+            },
+        });
     };
 
     return (
         <SearchContext.Provider
             value={{
-                title,
-                isLoading,
-                isEnd,
+                title: state.title,
+                isLoading: state.isLoading,
+                isEnd: state.isEnd,
                 handleSearch,
             }}
         >
