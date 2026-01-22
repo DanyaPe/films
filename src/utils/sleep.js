d71e88c349f1032aa4c457f9ba5b08dda8f7747e
DanyaPe <denzed5221@gmail.com>
2026-01-22 12:59:17 +0700

    docs: Добавление описания(JSDocs) функции sleep

--- src/utils/sleep.js
+++ src/utils/sleep.js
@@ -1,3 +1,8 @@
+/**
+ * Пауза между асинхронными действиями
+ * @param {Number} ms - Задержка в миллисекундах
+ * @returns {Promise}
+ */
 export default function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
